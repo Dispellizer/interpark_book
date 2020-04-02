671c0e702f8ad3625a7c5bf237efd0565e080db4
dispellizer <dispellizer@naver.com>
2020-04-02 12:22:10 +0900

    Updates

---
+++ precache-manifest.707c3ba700a76e70865638d890aaf475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b342dce99e4f2c7b1dd4e2da3f8a03fa",
+    "url": "/interpark_book/index.html"
+  },
+  {
+    "revision": "13ce00928d0a7e04f613",
+    "url": "/interpark_book/static/css/main.7afbb0a1.chunk.css"
+  },
+  {
+    "revision": "5888425d37622bd8a02e",
+    "url": "/interpark_book/static/js/2.730e3387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/interpark_book/static/js/2.730e3387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ce00928d0a7e04f613",
+    "url": "/interpark_book/static/js/main.c92757ae.chunk.js"
+  },
+  {
+    "revision": "62bae104a5e18774a2c9",
+    "url": "/interpark_book/static/js/runtime-main.3b144027.js"
+  }
+]);
